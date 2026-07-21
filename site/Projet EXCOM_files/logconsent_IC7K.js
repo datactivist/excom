92e975a3ff7d633ca2ee3691309ec17cb174673c
DianeThy <diane.thierry.3asd@gmail.com>
2026-07-21 16:39:46 +0200

    report site readymag sur repo

---
+++ site/Projet EXCOM_files/logconsent_IC7K.js
@@ -0,0 +1,3 @@
+CookieConsent.setCookie("{stamp:%27E9zC5xACSB5MOJ84GW6bSeysaIdfMPkKe2rNKAtz5/pau2Y2a8RGeA==%27%2Cnecessary:true%2Cpreferences:false%2Cstatistics:false%2Cmarketing:false%2Cmethod:%27explicit%27%2Cver:1%2Cutc:1784540482964%2Cregion:%27es%27}", new Date(1816076482964), '/', '', '');
+if (typeof CookieConsent.removeBulkReset!='undefined') {CookieConsent.removeBulkReset();}
+CookieConsent.userCountry = 'es';
